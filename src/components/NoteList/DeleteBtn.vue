a9d9603e524523e0abe3d1ff606f5e9da2bc3111
kimura keisuke <mail@kkeisuke.com>
2023-01-22 23:20:56 +0900

    DeleteBtn 追加

---
+++ src/components/NoteList/DeleteBtn.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import xSquareFill from '@/assets/icons/bi-x-square-fill.svg?url'
+
+const emit = defineEmits<{
+  (e: 'delete', value: boolean): void
+}>()
+
+const confirm = ref(false)
+const LIMIT = 5000
+
+const onClick = () => {
+  if (!confirm.value) {
+    confirm.value = true
+    setTimeout(() => (confirm.value = false), LIMIT)
+    return
+  }
+  emit('delete', confirm.value)
+}
+</script>
+
+<template>
+  <svg role="button" class="delete-btn" :class="{ 'text-danger': confirm }" @click.stop="onClick">
+    <use :href="`${xSquareFill}#icon`" />
+  </svg>
+</template>
+
+<style scoped>
+.delete-btn {
+  width: 1rem;
+  height: 1rem;
+  background-color: #fff;
+}
+</style>
